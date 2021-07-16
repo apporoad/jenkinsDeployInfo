166f9e5375419628c82b109a465fedf6c4d9a45a
liuxieyong <liuxieyong@tripwise.cn>
2021-07-16 10:15:08 +0800

    some

--- api/collect.js
+++ api/collect.js
@@ -2,6 +2,7 @@ var configs = require('../collect.config.js')
 var lisaP = require('lisa.promise')
 var req = require('mini.req.js')
 var moment = require('moment')
+var deployInfo = require('./index')
 
 function drawNum(str){
     var numArr = str.match(/\d+/g)
@@ -11,9 +12,18 @@ function drawNum(str){
     return null
 }
 
+var getDeployLastVersion = function(node){
+    var versions = deployInfo.deploy['@get']({ node : node})
+    if(versions.length>1){
+        return versions[1]
+    }
+    return null
+}
+
 module.exports = {
     "@get": async function(params){
         const LiSAP = lisaP(2)
+
         LiSAP.assignBatch(async one => {
             var result = Object.assign({},one)
 
@@ -23,14 +33,20 @@ module.exports = {
             result.commitId = result.commitId.length < 50 ? result.commitId : 'error'
             result.tempVersion = result.tempVersion.length < 50 ? result.tempVersion : 'error'
 
+            //添加距离时间
             var dateStr = drawNum(result.tempVersion)
             if(dateStr){
                 result.fromNow = moment(dateStr, "YYYYMMDDhhmmss").fromNow()
             }else{
                 result.fromNow = '-'
             }
-
-            
+            var lv = getDeployLastVersion(result.name)
+            if(result.tempVersion && lv && result.tempVersion.indexOf(lv) > -1){
+                result.lastVersion= '已是最新版本'
+            }
+            else{
+                result.lastVersion = lv
+            }
             return result
         },configs.nodes)
         var results = await LiSAP.action()
